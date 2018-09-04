1bb8e658715d0f11deddb7bd03c96bf57042e6a2
RockQ <1058737674@qq.com>
2018-09-04 10:46:12 +0800

    提交测试demo

---
+++ MavenDemo/app/src/main/java/cn/cestco/mavendemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package cn.cestco.mavendemo
+
+import android.os.Bundle
+import com.cesecsh.baselib.ui.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Loginac
+    }
+}
